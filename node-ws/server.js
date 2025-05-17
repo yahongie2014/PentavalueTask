1631584559c91d3264e4517c7620a0f6b11705c9
asaeed <ahmed.awad@future-group.com>
2025-05-17 16:42:06 +0300

    socket local and prod

--- node-ws/server.js
+++
@@ -1,8 +0,0 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({port: process.env.PORT || 3000});
-
-wss.on('connection', ws => {
-    console.log('Client connected');
-    ws.on('message', message => console.log('Received:', message));
-    ws.send('Welcome!');
-});
